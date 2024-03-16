2c87c5a58c9666606acf4644ef5cb124313ce4ae
Никита Зивенко <zivenko@MacBook-Air-Nikita.local>
2024-03-16 16:46:28 +0300

    Solve task find-element-index

--- delete-duplicates.py
+++ delete-duplicates.py
@@ -1,9 +1,5 @@
 length = int(input())
-
 unique_dict = dict.fromkeys(input().split(' '))
-
 appendix = length - len(unique_dict)
-
 result = ' '.join(unique_dict) + ' _' * appendix
-
 print(result)
--- find-element-index.py
+++ find-element-index.py
@@ -1,9 +1,16 @@
-length = int(input())
+def find_or_guess_index(input_numbers: list, req_number: int):
+    left = 0
+    right = len(input_numbers)
+    while left < right:
+        mid = (left + right) // 2
+        if input_numbers[mid] == req_number:
+            return input_numbers.index(req_number)
+        if input_numbers[mid] < req_number:
+            left = mid + 1
+        else:
+            right = mid
+    if req_number > input_numbers[mid]:
+        return mid + 1
+    return mid
 
-unique_dict = dict.fromkeys(input().split(' '))
-
-appendix = length - len(unique_dict)
-
-result = ' '.join(unique_dict) + ' _' * appendix
-
-print(result)
+print(find_or_guess_index(list(map(int, input().split())), int(input())))
